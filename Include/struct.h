57c1d375bb8d0afff253181b551b3805d783b29b
Hp de moa <taquettom3@gmail.com>
2024-06-25 19:18:35 +0200

    + | created struct.h and created AST's struct

--- Include/struct.h
+++ Include/struct.h
@@ -1,14 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/25 16:19:25 by tom               #+#    #+#             */
-/*   Updated: 2024/06/25 19:15:14 by tom              ###   ########.fr       */
+/*   Created: 2024/06/25 19:13:38 by tom               #+#    #+#             */
+/*   Updated: 2024/06/25 19:17:35 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libamoa.h"
-#include "struct.h"
+typedef struct s_ast
+{
+	t_ast_content *base;
+	struct s_ast *gauche;
+	struct s_ast *droite;
+}	t_ast;
+
+typedef struct s_ast_content
+{
+	bool is_op;
+	char *cmd_op;
+	bool builtins;
+	bool flags;
+	char *file_name;
+}	t_ast_content;
