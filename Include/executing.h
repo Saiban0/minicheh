06999c60ac30f762c3051faea4fa905625feebbd
Saiban0 <Saiban0@github.com>
2024-06-26 18:38:57 +0200

    + | cmd is now char **

--- Include/executing.h
+++ Include/executing.h
@@ -6,14 +6,14 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 19:44:47 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/25 20:06:35 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/26 17:31:02 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTING_H
 # define EXECUTING_H
 
-void	exec_simple(char *cmd, char **envp);
+void	exec_simple(t_ast *cmd, char **envp);
 int		open_file(char *file, int in_or_out);
 int		check_dir(char *cmd);
 char	*ft_getpath(char *cmd);
