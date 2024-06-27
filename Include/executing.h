5c0cd9ec4f86ea090417e9074c08bab1314205e1
Saiban0 <Saiban0@github.com>
2024-06-27 15:46:02 +0200

    ~ | commands are now executed in a child process

--- Include/executing.h
+++ Include/executing.h
@@ -6,15 +6,17 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 19:44:47 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/26 17:31:02 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/27 15:42:48 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTING_H
 # define EXECUTING_H
 
-void	exec_simple(t_ast *cmd, char **envp);
-int		open_file(char *file, int in_or_out);
+void	exec_switch(t_ast *cmd, char **envp);
+void	exec_extern(t_ast *cmd, char **envp);
+void	exec_child(t_ast *cmd, char **envp, char *cmd_path);
+int		open_file(char *file, int in_or_out, t_ast *cmd);
 int		check_dir(char *cmd);
 char	*ft_getpath(char *cmd);
 
--- src/executing/exec_simple.c
+++ src/executing/exec_simple.c
@@ -6,19 +6,48 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 19:16:54 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/26 18:32:14 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/06/27 15:45:42 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exec_simple(t_ast *cmd, char **envp)
+void	exec_switch(t_ast *cmd, char **envp)
+{
+	if (cmd->base->cmd[0][0] == '\0')
+		error_management(e_empty_command, cmd);
+	if (cmd->base->is_op)
+	{
+		// if (cmd->base->cmd_op == 1)
+		// 	handle_pipe(cmd, envp);
+		// if (cmd->base->cmd_op == 2)
+		// 	handle_redirect_input(cmd, envp);
+		// if (cmd->base->cmd_op == 3)
+		// 	handle_redirect_output(cmd, envp);
+	}
+	// else if (cmd->base->builtins)
+		// handle_builtins(cmd, envp);
+	else if (cmd->base->cmd_op == 0)
+		exec_extern(cmd, envp);
+}
+
+void	exec_extern(t_ast *cmd, char **envp)
 {
 	char	*cmd_path;
+	pid_t	new_pid;
+	int		status;
 
-	if (cmd->base->cmd[0][0] == '\0')
-		exit(0);
 	cmd_path = ft_getpath(cmd->base->cmd[0]);
+	new_pid = fork();
+	if (new_pid == -1)
+		error_management(e_fork_failure, cmd);
+	if (new_pid == 0)
+		exec_child(cmd, envp, cmd_path);
+	waitpid(new_pid, &status, 0);
+}
+
+void	exec_child(t_ast *cmd, char **envp, char *cmd_path)
+{
 	if (execve(cmd_path, cmd->base->cmd, envp) == -1)
 	{
 		free(cmd_path);
