adcdf58e38d3aeb139ab6a6109166b39088bc429
Saiban0 <Saiban0@github.com>
2024-09-30 19:48:52 +0200

    + | new error codes and frees

--- src/error_managent/error_management.c
+++ src/error_managent/error_management.c
@@ -6,30 +6,40 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 21:21:04 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/28 16:22:26 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/30 16:49:15 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	error_free(t_ast *cmd)
+void	error_free(t_ast *cmd, t_pipex *pipex)
 {
+	if (pipex)
+	{
+		if (pipex->pipe_fd)
+			ft_free_double_array(((char **)pipex->pipe_fd));
+		if (pipex->path_list)
+			ft_free_double_array(pipex->path_list);
+		free(pipex);
+	}
 	if (cmd->left)
-		error_free(cmd->left);
+		error_free(cmd->left, pipex);
 	if (cmd->right)
-		error_free(cmd->right);
+		error_free(cmd->right, pipex);
 	if (cmd->base)
 	{
 		if (cmd->base->cmd)
 			ft_free_double_array(cmd->base->cmd);
 		if (cmd->base->file_name)
 			free(cmd->base->file_name);
+		if (cmd->base->path)
+			free(cmd->base->path);
 		free(cmd->base);
 	}
 	free(cmd);
 }
 
-void	error_management(int error_code, t_ast *cmd)
+void	error_management(int error_code, t_ast *cmd, t_pipex *pipex)
 {
 	if (error_code == 1)
 	{
@@ -38,7 +48,7 @@ void	error_management(int error_code, t_ast *cmd)
 	}
 	if (error_code == 2)
 	{
-		ft_putstr_fd("minicheh: file not found: ", STDERR_FILENO);
+		ft_putstr_fd("minicheh: can't open file: ", STDERR_FILENO);
 		ft_putendl_fd(cmd->base->cmd[0], STDERR_FILENO);
 	}
 	if (error_code == 3)
@@ -47,15 +57,20 @@ void	error_management(int error_code, t_ast *cmd)
 	{
 		ft_putstr_fd("minicheh : fork failure on command: ", STDERR_FILENO);
 		ft_putendl_fd(cmd->base->cmd[0], STDERR_FILENO);
-		error_free(cmd);
+		error_free(cmd, pipex);
 		exit(-1);
 	}
 	if (error_code == 5)
 	{
 		ft_putstr_fd("minicheh : pipe failure\n", STDERR_FILENO);
-		error_free(cmd);
+		error_free(cmd, pipex);
 		exit(-1);
 	}
-	error_free(cmd);
+	if (error_code == 6)
+		ft_putstr_fd("minicheh : problem while searching in env\n",
+			STDERR_FILENO);
+	if (error_code == 7)
+		ft_putstr_fd("minicheh : malloc failure\n", STDERR_FILENO);
+	error_free(cmd, pipex);
 	exit(0);
 }
