30106cd91b58e3df64763cce8151a8ee93c1207e
Hp de moa <taquettom3@gmail.com>
2024-09-30 15:02:19 +0200

    + | Added ft_echo.c

---
+++ src/builtins/ft_echo.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 09:03:45 by tom               #+#    #+#             */
+/*   Updated: 2024/09/30 15:01:54 by tom              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_echo(char **arg)
+{
+	char	*to_write;
+	bool	flag;
+	int		i;
+	
+	arg++;
+	i = -1;
+	flag = false;
+	to_write = ft_calloc(1, 1);
+	if (arg[0][0] == '-' && arg[0][1] == 'n')
+	{
+		flag = true;
+		arg++;
+	}
+	while (arg[++i])
+	{
+		to_write = ft_strjoin(to_write, arg[i], 1);
+		to_write[ft_strlen(to_write)] = ' ';
+	}
+	to_write[ft_strlen(to_write)] = '\0';
+	to_write[ft_strlen(to_write) - 1] = '\n';
+	if (flag)
+		to_write[ft_strlen(to_write) - 1] = '\0';
+	write(1, &(*to_write), ft_strlen(to_write) + 1);
+	return (to_write);
+}
