1da0bde47c8f6596c235024ced47aa6ea321c722
Saiban0 <Saiban0@github.com>
2024-09-30 19:46:55 +0200

    + | added enum values

--- Include/error_management.h
+++ Include/error_management.h
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 21:21:40 by bchedru           #+#    #+#             */
-/*   Updated: 2024/06/28 14:33:52 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/24 18:52:32 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 
 typedef enum s_error_management
 {
-	e_none,
-	e_command_not_found,
-	e_file_not_found,
-	e_empty_command,
-	e_fork_failure,
-	e_pipe_failure,
+	e_none = 0,
+	e_command_not_found = 1,
+	e_file = 2,
+	e_empty_command = 3,
+	e_fork_failure = 4,
+	e_pipe_failure = 5,
+	e_env_error =6,
+	e_malloc_failure = 7,
 }	t_error;
 
-void	error_management(int error_code, t_ast *cmd);
-
+void	error_management(int error_code, t_ast *cmd, t_pipex *pipex);
 #endif
