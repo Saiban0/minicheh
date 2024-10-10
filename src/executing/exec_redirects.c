0e53a742d5ad211ce3d4d44730d059ec25fcb473
Saiban0 <Saiban0@github.com>
2024-10-10 17:21:23 +0200

    + | Prevented infinite loop with correct recursive

--- src/executing/exec_redirects.c
+++ src/executing/exec_redirects.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:52:05 by bchedru           #+#    #+#             */
-/*   Updated: 2024/10/09 14:01:02 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/10/10 16:47:08 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	search_redirects(t_ast *ast, t_pipex *pipex)
 {
 	if (ast->left)
-		search_redirects(ast, pipex);
+		search_redirects(ast->left, pipex);
 	if (ast->right)
-		search_redirects(ast, pipex);
-	if (ast->base->cmd_op == e_redirect_input)
+		search_redirects(ast->right, pipex);
+	if (ast->base->cmd_op == e_redirect_input && ast->left->base->file_name)
 		pipex->in_file = ast->left->base->file_name;
-	if (ast->base->cmd_op == e_redirect_output)
+	if (ast->base->cmd_op == e_redirect_output && ast->right->base->file_name)
 		pipex->out_file = ast->right->base->file_name;
 }
