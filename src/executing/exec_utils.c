6e341bc2ead6f63b259658870ec85045defa8b16
Saiban0 <Saiban0@github.com>
2024-10-14 14:37:56 +0200

    - Uncommented used builtins

--- src/executing/exec_utils.c
+++ src/executing/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 19:25:44 by bchedru           #+#    #+#             */
-/*   Updated: 2024/10/11 12:50:42 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/10/11 16:57:06 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@ void	exec_builtins(t_ast *cmd, t_env *env)
 	{
 		if (cmd->base->cmd_op == e_echo)
 			ft_echo(cmd->base->cmd);
-		// if (cmd->base->cmd_op == e_cd)
-			// ft_cd(cmd->base->cmd);
-		// if (cmd->base->cmd_op == e_pwd)
-			// ft_pwd(cmd->base->cmd);
+		if (cmd->base->cmd_op == e_cd)
+			ft_cd(cmd->base->cmd, &env);
+		if (cmd->base->cmd_op == e_pwd)
+			ft_pwd(cmd->base->cmd, env);
 		// if (cmd->base->cmd_op == e_export)
 		// 	ft_export(cmd->base->cmd);
 		// if (cmd->base->cmd_op == e_unset)
 		// 	ft_unset(cmd->base->cmd);
 		// if (cmd->base->cmd_op == e_exit)
 		// 	ft_exit(cmd->base->cmd);
-		// if (cmd->base->cmd_op == e_env)
-		// 	ft_env(cmd->base->cmd);
+		if (cmd->base->cmd_op == e_env)
+			ft_env(cmd->base->cmd, env);
 		exit(EXIT_SUCCESS);
 	}
 }
