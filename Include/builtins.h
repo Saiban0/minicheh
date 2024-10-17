e7a3977799070ee2a4ef2b8ee2e74be95a7e0112
Hp de moa <taquettom3@gmail.com>
2024-10-17 13:23:45 +0200

    + | Added ft_unset

--- Include/builtins.h
+++ Include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 09:04:41 by tom               #+#    #+#             */
-/*   Updated: 2024/10/15 17:44:46 by tom              ###   ########.fr       */
+/*   Updated: 2024/10/17 13:16:00 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,19 @@ bool			ft_cd(char	**arg, t_env	**env);
 
 void			ft_pwd(char	**arg, t_env	*env);
 
-bool			check_arg(char	*arg, t_env	**env);
+/**
+ * @brief Return whether the string contain a special character or not.
+ * 
+ * @param str The string.
+ * @return true The string contain a special character.
+ * @return false The string not contain a special character.
+ */
+bool			check_special_char(char	*str);
+bool			check_export_arg(char	*arg, t_env	**env);
 char			**double_array_cat(char	**tab1, char	**tab2);
-bool			ft_find(char	*arg, t_env	**env, char	**to_find);
+bool			ft_find_export(char	*arg, t_env	**env, char	**to_find);
 void			ft_export(char	**arg, t_env	**env);
 
+void			ft_unset(char	**arg, t_env	**env);
 
 #endif
