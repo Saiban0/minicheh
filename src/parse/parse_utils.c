d0183938bb5c9093c3cc690eda4589863ebb703d
Hp de moa <taquettom3@gmail.com>
2024-10-18 14:38:50 +0200

    + | Added quote handler

--- src/parse/parse_utils.c
+++ src/parse/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:30:08 by tom               #+#    #+#             */
-/*   Updated: 2024/10/11 12:43:08 by tom              ###   ########.fr       */
+/*   Updated: 2024/10/18 14:09:30 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,15 @@ char	*rem_wspace(char *command)
 	if (is_whitespace(command[i]))
 		command[i] = '\0';
 	return (command);
+}
+
+void	quote_handler(char	*line, t_env	**env, int	quote_pos)
+{
+	if ((*env)->quote == 0)
+		{(*env)->quote = line[quote_pos];ft_printf("quote open\n");}
+	else if ((*env)->quote == line[quote_pos])
+		{(*env)->quote = 0;ft_printf("quote close\n");}
+	else if ((*env)->quote != 0 && (*env)->quote != line[quote_pos])
+		return ;
+	return ;
 }
