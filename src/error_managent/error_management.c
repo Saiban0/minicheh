d9fb2fc7bdcd4c6b868dc56ded81d442e681f770
Saiban0 <bartched@outlook.fr>
2024-10-23 17:10:25 +0200

    ~ | error_management now uses ft_exit at the correct times instead of error_free

--- src/error_managent/error_management.c
+++ src/error_managent/error_management.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 21:21:04 by bchedru           #+#    #+#             */
-/*   Updated: 2024/10/21 20:10:28 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/10/23 16:59:07 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	error_free(t_ast *cmd, t_pipex *pipex)
 		free(pipex);
 		pipex = NULL;
 	}
-	free_ast(cmd);
+	if (cmd)
+		free_ast(cmd);
 }
 
 static void	error_management_bis(int error_code, t_ast *cmd, t_pipex *pipex)
@@ -38,12 +39,13 @@ static void	error_management_bis(int error_code, t_ast *cmd, t_pipex *pipex)
 		ft_putstr_fd("minicheh : malloc failure\n", STDERR_FILENO);
 }
 
-void	error_management(int error_code, t_ast *cmd, t_pipex *pipex)
+void	error_management(int error_code, t_ast *cmd, t_pipex *pipex, t_env *env)
 {
 	if (error_code == 1)
 	{
 		ft_putstr_fd("minicheh: command not found: ", STDERR_FILENO);
 		ft_putendl_fd(cmd->base->cmd[0], STDERR_FILENO);
+		ft_exit(NULL, cmd, env, pipex);
 	}
 	if (error_code == 2)
 	{
@@ -56,7 +58,7 @@ void	error_management(int error_code, t_ast *cmd, t_pipex *pipex)
 	{
 		ft_putstr_fd("minicheh : fork failure on command: ", STDERR_FILENO);
 		ft_putendl_fd(cmd->base->cmd[0], STDERR_FILENO);
-		error_free(cmd, pipex);
+		ft_exit(NULL, cmd, env, pipex);
 	}
 	error_management_bis(error_code, cmd, pipex);
 	error_free(cmd, pipex);
