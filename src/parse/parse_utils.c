a2ab5a8cbafe877d5f6892db5ceee06d37286094
Hp de moa <taquettom3@gmail.com>
2024-10-25 16:25:00 +0200

    ~ | Updated to 42's norm

--- src/parse/parse_utils.c
+++ src/parse/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:30:08 by tom               #+#    #+#             */
-/*   Updated: 2024/10/25 16:04:52 by tom              ###   ########.fr       */
+/*   Updated: 2024/10/25 16:08:41 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,49 +63,6 @@ int	quote_test(int	c, int	quote)
 	return (-1);
 }
 
-int	result_length(char const *str, char c)
-{
-	int		i;
-	int		res;
-	bool	is_sep;
-	int		quote;
-
-	i = -1;
-	quote = 0;
-	res = 0;
-	is_sep = true;
-	while (str[++i])
-	{
-		if (str[i] == '"' || str[i] == '\'')
-		{
-			quote = quote_test(str[i], quote);
-			if (quote == 0)
-				res++;
-		}
-		else if (is_sep && str[i] != c && quote <= 0)
-		{
-			res++;
-			is_sep = false;
-		}
-		if (str[i] == c && quote <= 0)
-			is_sep = true;
-	}
-	return (res);
-}
-
-char	*cuted(char const *str, int end)
-{
-	char	*res;
-	int		i;
-
-	i = -1;
-	res = ft_calloc(1, end + 2);
-	while (str[++i] && i <= end)
-		res[i] = str[i];
-	res[i] = '\0';
-	return (res);
-}
-
 bool	only_wspace(char	*str)
 {
 	int	i;
@@ -115,83 +72,4 @@ bool	only_wspace(char	*str)
 		if (!is_whitespace(str[i]))
 			return (false);
 	return (true);
-}
-
-char	**clear_res(char	**d_array)
-{
-	char	**res;
-	char	*temp_str;
-	int		temp_int;
-	int		i;
-
-	temp_int = 0;
-	i = -1;
-	while (d_array[++i])
-	{
-		if (only_wspace(d_array[i]))
-			d_array[i][0] = ' ';
-		else
-		{
-			temp_str = rem_wspace(d_array[i]);
-			free(d_array[i]);
-			d_array[i] = ft_strdup(temp_str);
-			free(temp_str);
-			temp_int++;
-		}
-	}
-	i = -1;
-	res = ft_calloc(temp_int + 2, sizeof(char *));
-	temp_int = -1;
-	while (d_array[++i])
-		if (d_array[i][0] != ' ')
-			res[++temp_int] = ft_strdup(d_array[i]);
-	ft_free_double_array(d_array);
-	return (res);
-}
-
-char	**ft_split_arg(char *str, char sep)
-{
-	int		len;
-	char	**res;
-	int		quote;
-	int		i;
-	int		j;
-
-	i = -1;
-	j = -1;
-	quote = 0;
-	len = result_length(str, sep);
-	res = ft_calloc((len + 2), sizeof(char *));
-	if (!res)
-		return (NULL);
-	while (str[++i] && j < len)
-	{
-		if (str[i] == '"' || str[i] == '\'')
-		{
-			quote = quote_test(str[i], quote);
-			if (quote == 0)
-			{
-				res[++j] = cuted(str, i - 1);
-				if (res[j][0] == '"' || res[j][0] == '\'')
-					res[j][0] = ' ';
-				str += i + 1;
-				i = -1;
-			}
-		}
-		if (str[i] == sep && quote <= 0)
-		{
-			res[++j] = cuted(str, i - 1);
-			str += i + 1;
-			i = -1;
-		}
-	}
-	if (j < len)
-	{
-		quote = quote_test(str[i], quote);
-		res[++j] = cuted(str, i - 1);
-	}
-	res[++j] = NULL;
-	res = clear_res(res);
-	//faire une fonction qui utilise rem_wspace dans chaque str de res et qui enlève les noeuds vide
-	return (res);
 }
