89d69f8e7a512b12689df85310a298d3bd486ba5
Hp de moa <taquettom3@gmail.com>
2024-10-25 15:37:59 +0200

    ~ | Updated is_builtins

--- src/parse/parsing_test.c
+++ src/parse/parsing_test.c
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 02:52:39 by tom               #+#    #+#             */
-/*   Updated: 2024/10/24 16:53:29 by tom              ###   ########.fr       */
+/*   Updated: 2024/10/25 15:30:38 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,23 @@ bool	is_whitespace(char	c)
 
 t_cmd_and_op	is_builtins(char *command)
 {
-	char			*temp;
 	t_cmd_and_op	res;
 
-	temp = rem_wspace(command);
-	if (ft_strncmp(temp,"echo", 4) == 0)
+	if (ft_strncmp(command,"echo", 4) == 0)
 		res = e_echo;
-	if (ft_strncmp(temp,"cd", 2) == 0)
+	else if (ft_strncmp(command,"cd", 2) == 0)
 		res = e_cd;
-	if (ft_strncmp(temp,"pwd", 3) == 0)
+	else if (ft_strncmp(command,"pwd", 3) == 0)
 		res = e_pwd;
-	if (ft_strncmp(temp,"export", 6) == 0)
+	else if (ft_strncmp(command,"export", 6) == 0)
 		res = e_export;
-	if (ft_strncmp(temp,"unset", 5) == 0)
+	else if (ft_strncmp(command,"unset", 5) == 0)
 		res = e_unset;
-	if (ft_strncmp(temp,"exit", 4) == 0)
+	else if (ft_strncmp(command,"exit", 4) == 0)
 		res = e_exit;
-	if (ft_strncmp(temp,"env", 3) == 0)
+	else if (ft_strncmp(command,"env", 3) == 0)
 		res = e_env;
 	else 
 		res = e_external_control;
-	free(temp);
 	return (res);
 }
