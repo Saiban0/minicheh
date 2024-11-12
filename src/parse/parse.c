471bffcfa5113c4e0467adc4e2d1a7846df1b296
Hp de moa <taquettom3@gmail.com>
2024-11-12 14:54:26 +0100

    -~| Removed select_operator, open_quote and Updated parse and add_env

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 19:12:58 by tom               #+#    #+#             */
-/*   Updated: 2024/11/05 17:34:14 by tom              ###   ########.fr       */
+/*   Updated: 2024/11/07 17:30:02 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,6 @@ void	without_op(char *line, t_ast	**ast)
 	(*ast)->base->cmd_op = is_builtins((*ast)->base->cmd[0]);
 	(*ast)->base->builtins = (*ast)->base->cmd_op >= e_echo;
 	free(temp);
-}
-
-bool	select_operator(char	*line, int	i, t_ast	**ast)
-{
-	int	j;
-
-	j = i + 1;
-	while (line[j] && is_whitespace(line[j]) == true)
-		j++;
-	if (!line[j])
-		return (false);
-	// A gérer le cas ou c'est un pipe en fin de ligne
-	// Même fonctionnement que avec un " en fin de ligne
-	// (Ouvrir les guillemets)
-	if (line[i] == '|' && line[i + 1] == '|')
-		ft_exit(line, *ast, *(*ast)->t_env, NULL);
-	else if (line[i] == '|')
-		ast_pipe(line, i, ast);
-	else if (line[i] == '<' && line[i + 1] == '<')
-		ast_else(line, i + 1, ast, e_here_doc);
-	else if (line[i] == '>' && line[i + 1] == '>')
-		ast_else(line, i + 1, ast, e_redirect_output_write_mod);
-	else if (line[i] == '>')
-		ast_else(line, i, ast, e_redirect_output);
-	else if (line[i] == '<')
-		ast_else(line, i, ast, e_redirect_input);
-	return (true);
 }
 
 char	*find_env_var(char	*var, char	**envv)
@@ -71,10 +44,11 @@ void	add_env(t_env	**env_start, t_ast	**ast)
 	int		i;
 	char	*temp;
 	
-	(*env_start)->nb_commands += ((*ast)->base->cmd_op == e_external_control)
-		|| ((*ast)->base->cmd_op >= e_echo);
+	i = -1;
 	(*ast)->t_env = env_start;
 	(*ast)->base->path = NULL;
+	(*env_start)->nb_commands += ((*ast)->base->cmd_op == e_external_control)
+		|| ((*ast)->base->cmd_op >= e_echo);
 	if ((*ast)->base->cmd_op != e_file_name)
 		(*ast)->base->file_name = NULL;
 	if (!(((*ast)->base->cmd_op == e_external_control)
@@ -82,7 +56,6 @@ void	add_env(t_env	**env_start, t_ast	**ast)
 		(*ast)->base->cmd = NULL;
 	else
 	{
-		i = -1;
 		while ((*ast)->base->cmd[++i])
 		{
 			if ((*ast)->base->cmd[i][0] == '$' && (*ast)->base->cmd[i][1] != '(')
@@ -100,35 +73,22 @@ void	add_env(t_env	**env_start, t_ast	**ast)
 		add_env(env_start, &(*ast)->right);
 }
 
-void	open_quote(char	*text, t_ast	**ast, t_env	*env, char	*oldline)
-{
-	char	*newline;
-	char	*temp;
-	t_ast	*last_node;
-
-	temp = readline(text);
-	newline = ft_calloc(ft_strlen(temp) + ft_strlen(oldline) + 1, sizeof(char));
-	ft_strlcat(newline, oldline, ft_strlen(temp) + ft_strlen(oldline) + 1);
-	ft_strlcat(newline, temp, ft_strlen(temp) + ft_strlen(oldline) + 1);
-	last_node = *ast;
-	while (last_node->right)
-		last_node = last_node->right;
-	if (text[0] == 'd')
-		parse(newline, ast, env, 0);
-	if (text[0] == 'q')
-		parse(newline, ast, env, 0);
-	free(newline);
-}
-
 void	parse(char *line, t_ast	**ast, t_env	*env, int	quote)
 {
 	int		i;
+	int		quote_pipe_res;
 
 	i = -1;
 	(*ast)->base->cmd_op = e_empty;
 	(*ast)->left = NULL;
 	(*ast)->right = NULL;
 	(*ast)->t_env = &env;
+	quote_pipe_res = quote_pipe_check(line);
+	if (quote_pipe_res != 0)
+	{
+		text_open_quote_select(line, ast, env, quote_pipe_res);
+		return ;
+	}
 	while (line[++i])
 	{
 		if (line[i] == '"' || line[i] == '\'')
@@ -137,8 +97,7 @@ void	parse(char *line, t_ast	**ast, t_env	*env, int	quote)
 		{
 			if (select_operator(line, i, ast) == false)
 			{
-				ft_putstr_fd("minishell: ", STDERR_FILENO);
-				ft_putstr_fd("syntax error near unexpected token `newline'\n", STDERR_FILENO);
+				parse_error_handler(e_unexpected_newline, ast);
 				return ;
 			}
 			line += i + (line[i] == line[i + 1]);
