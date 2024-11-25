bd54edf17c643422d868fd0664868b1145645333
Saiban0 <bartched@outlook.fr>
2024-11-25 20:13:45 +0100

    + | Wrote a function to join back the split command

--- src/parse/env_var_handler.c
+++ src/parse/env_var_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_var_handler.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttaquet <ttaquet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:14:57 by ttaquet           #+#    #+#             */
-/*   Updated: 2024/11/25 17:23:48 by ttaquet          ###   ########.fr       */
+/*   Updated: 2024/11/25 20:12:30 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,53 @@ char	**ft_split_env_arg(char *command, int res_size, char **envv)
 	}
 	res[j] = NULL;
 	ft_print_double_array(res);
+	return (res);
+}
+
+// Compte le nombre de caracteres au total dans le double tableau
+// Il y a moyen que ca merde et compte les caractere nuls et donne un resultat plus grand que prevu
+// J'ai pas teste parceque je sais pas comment tu comptes finir ta fonction env_var_handler
+static int	caca_feur(char **array)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (array[i])
+	{
+		while (*array[i])
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+char	*ft_join_env_arg(char **array)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	res = malloc(caca_feur(array) * sizeof(char));
+	while (array[i])
+	{
+		j = 0;
+		while (array[i][j])
+		{
+			res[k] = array[i][j];
+			j++;
+			k++;
+		}
+		free(array[i]);
+		i++;
+	}
+	free(array);
+	res[k] = '\0';
 	return (res);
 }
 
