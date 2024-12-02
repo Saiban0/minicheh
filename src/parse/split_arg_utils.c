f5a98ee2765f1c807909ec88baebf28e67079ce8
Hp de moa <taquettom3@gmail.com>
2024-12-02 17:15:01 +0100

    ~ | Upgraded quote and dquote handler

--- src/parse/split_arg_utils.c
+++ src/parse/split_arg_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split_arg_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttaquet <ttaquet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 14:13:14 by ttaquet           #+#    #+#             */
-/*   Updated: 2024/11/20 12:57:56 by ttaquet          ###   ########.fr       */
+/*   Updated: 2024/12/02 16:26:53 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	result_length(char const *str)
 {
 	int		i;
 	int		res;
-	bool	is_sep;
 	int		quote;
+	bool	is_sep;
 
 	i = -1;
 	quote = 0;
@@ -37,4 +37,36 @@ int	result_length(char const *str)
 			is_sep = true;
 	}
 	return (res - 1);
+}
+
+int *result_quote_tab(const char *str)
+{
+	int		*res;
+	int		i;
+	int		j;
+	int		quote;
+	bool	is_sep;
+
+	j = -1;
+	i = -1;
+	quote = 0;
+	is_sep = true;
+	res = ft_calloc(result_length(str), sizeof(int));
+	while (str[++i])
+	{
+		if (str[i] == '"' || str[i] == '\'')
+		{
+			quote = quote_test(str[i], quote);
+			if (quote == 0)
+				res[++j] = str[i];
+		}
+		else if (is_sep && !is_whitespace(str[i]) && quote <= 0)
+		{
+			res[++j] = 0;
+			is_sep = false;
+		}
+		if (is_whitespace(str[i]) && quote <= 0)
+			is_sep = true;
+	}
+	return (res);
 }
