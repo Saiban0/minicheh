e63b1eb59da62b8a4839afd5c1a870bd8d4bb810
Hp de moa <taquettom3@gmail.com>
2024-12-17 15:47:49 +0100

    ~ | Updated split_arg

--- src/parse/split_arg.c
+++ src/parse/split_arg.c
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 16:07:13 by tom               #+#    #+#             */
-/*   Updated: 2024/12/13 15:54:55 by tom              ###   ########.fr       */
+/*   Updated: 2024/12/17 15:37:26 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,14 +49,21 @@ int	*init_tab_int(void)
 
 void	split_quote_handler(char **res, int *tab_int, char *str)
 {
-	ft_printf("--");
+	bool	is_cuted;
+
+	is_cuted = false;
 	tab_int[3] = quote_test(tab_int[0], tab_int[3], str);
+	if (tab_int[3] != 0 && str[tab_int[0]] != tab_int[3])
+		return ;
 	if (tab_int[3] == 0)
+	{
 		res[tab_int[2]] = cuted(str, tab_int[1] + 1, tab_int[0] - 1);
-	if (tab_int[3] == 0)
-	if (res[tab_int[2]] == NULL)
+		is_cuted = true;
+	}
+	if (res[tab_int[2]] == NULL && is_cuted)
 		res[tab_int[2]] = ft_strdup("");
-	tab_int[2]++;
+	if (is_cuted)
+		tab_int[2]++;
 	tab_int[1] = tab_int[0] + (tab_int[3] == 0);
 	tab_int[4] = (tab_int[3] == 0);
 }
@@ -67,7 +74,7 @@ char	**ft_split_arg(char *str)
 	int		*tab_int;
 	
 	tab_int = init_tab_int();
-	res = ft_calloc(result_length(str) + 2, sizeof(char *));
+	res = ft_calloc(result_length(str) + 2, 10 * sizeof(char*));
 	while (str[++tab_int[0]])
 	{
 		if (str[tab_int[0]] == '"' || str[tab_int[0]] == '\'')
