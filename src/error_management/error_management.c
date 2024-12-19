13ef7e610fa5091961226b3f5a9b03443a01b6fd
Tom Taquet <ttaquet@c1r3p3.42lehavre.fr>
2024-12-19 17:44:43 +0100

    ~ | Updated to 42 norm

--- src/error_management/error_management.c
+++ src/error_management/error_management.c
@@ -6,7 +6,7 @@
 /*   By: ttaquet <ttaquet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 21:21:04 by bchedru           #+#    #+#             */
-/*   Updated: 2024/12/19 15:32:10 by ttaquet          ###   ########.fr       */
+/*   Updated: 2024/12/19 17:44:10 by ttaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,28 @@ static void	error_management_bis(int error_code, t_ast *cmd, t_pipex *pipex,
 	}
 }
 
-void	error_management(t_error error_code, t_ast *cmd, t_pipex *pipex, t_env *env)
+void	error_management(t_error e_cod, t_ast *cmd, t_pipex *pipex, t_env *env)
 {
-	if (error_code == 1)
+	if (e_cod == 1)
 	{
 		ft_putstr_fd("minicheh: command not found: ", STDERR_FILENO);
 		ft_putendl_fd(cmd->base->cmd[0], STDERR_FILENO);
 		g_exit_code = CMDNOTFOUND;
 		ft_exit(NULL, cmd, env, pipex);
 	}
-	if (error_code == 2)
+	if (e_cod == 2)
 	{
 		ft_putstr_fd("minicheh: can't open file: ", STDERR_FILENO);
 		ft_putendl_fd(cmd->right->right->base->file_name, STDERR_FILENO);
 	}
-	if (error_code == 3)
+	if (e_cod == 3)
 		ft_putstr_fd("minicheh : empty command\n", STDERR_FILENO);
-	if (error_code == 4)
+	if (e_cod == 4)
 	{
 		ft_putstr_fd("minicheh : fork failure on command: ", STDERR_FILENO);
 		ft_putendl_fd(cmd->base->cmd[0], STDERR_FILENO);
 		ft_exit(NULL, cmd, env, pipex);
 	}
-	error_management_bis(error_code, cmd, pipex, env);
+	error_management_bis(e_cod, cmd, pipex, env);
 	error_free(cmd, pipex, env);
 }
